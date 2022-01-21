1ed30b7adfe75e2702c66570542271456aeef886
KimJiMin <minmin4736@naver.com>
2022-01-21 16:23:08 +0900

    🧪  :: 테스트

---
+++ Momentom/app.js
@@ -0,0 +1,8 @@
+const player = {
+  name: "test",
+  sayHello: function (otherPersonsName) {
+    console.log("Hello " + otherPersonsName + " nice to meet you");
+  },
+};
+
+player.sayHello("OwO");
