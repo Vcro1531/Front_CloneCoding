05350864cb1f00a521fc3ed5d95a43ae4bd4c908
KimJiMin <minmin4736@naver.com>
2022-02-09 19:34:13 +0900

    ⚡️ :: CSS & JS

--- Momentom/app.js
+++ Momentom/app.js
@@ -1,38 +1,12 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handletitleClick() {
-  h1.style.color = "blue";
-}
-
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here";
-}
-
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone";
-}
-
-function handleWindowResize() {
-  document.body.style.background = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier");
-}
-
-function handleWindowOffline() {
-  alert("Wifi Offline");
-}
-
-function handleWindowOnline() {
-  alert("Wifi Online");
+  const clickedClass = "clicked";
+  if (h1.className === clickedClass) {
+    h1.className = "";
+  } else {
+    h1.className = clickedClass;
+  }
 }
 
 h1.addEventListener("click", handletitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
