6fad2e4668ed36e463234c555b26efc61b016030
KimJiMin <minmin4736@naver.com>
2022-02-11 15:46:02 +0900

    ⚡️ :: handleLinkClick

--- Momentom/app.js
+++ Momentom/app.js
@@ -1,9 +1,17 @@
 const loginInput = document.querySelector("#login-form input");
 const loginForm = document.querySelector("#login-form");
 
-function onLoginSubmit() {
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
   const username = loginInput.value;
   console.log(username);
 }
 
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.log(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
