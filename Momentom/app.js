788a9da8728ce83cfb76ac1b09fe5152cd9fe872
KimJiMin <minmin4736@naver.com>
2022-02-11 15:33:10 +0900

    ⚡️ :: Submit Event

--- Momentom/app.js
+++ Momentom/app.js
@@ -1,9 +1,9 @@
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const loginForm = document.querySelector("#login-form");
 
-function handleLoginBtnClick() {
+function onLoginSubmit() {
   const username = loginInput.value;
   console.log(username);
 }
 
-loginButton.addEventListener("click", handleLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
