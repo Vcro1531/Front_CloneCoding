b7c2cea387a26e927281ca8b144324d6c3bb741e
KimJiMin <minmin4736@naver.com>
2022-02-21 19:48:00 +0900

    ⚡️ :: js -> html로 <img>

--- Momentom/js/background.js
+++ Momentom/js/background.js
@@ -1,3 +1,9 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg"];
 
-const changeimage=images[Math.floor(Math.random() * images.length]
+const changeimage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${changeimage}`;
+
+document.body.appendChild(bgImage);
