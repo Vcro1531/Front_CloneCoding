6aa0fa879656a6fb18ff6e2011cd89d3eb64026a
KimJiMin <minmin4736@naver.com>
2022-03-02 18:42:04 +0900

    ⚡️ :: Button

--- Momentom/js/todo.js
+++ Momentom/js/todo.js
@@ -5,8 +5,11 @@ const toDoList = document.getElementById("todo-list");
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
