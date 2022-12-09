2fb79f5248bff0028ab2266626a0b2c5e800b01c
Vcro1531 <jiminkim0520@gmail.com>
2022-12-09 11:52:04 +0900

    ⚡️ :: 굵기 변경 기능

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -1,8 +1,9 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 
 let isPainting = false;
 
@@ -19,9 +20,15 @@ function startPainting() {
 }
 function canclePainting() {
   isPainting = false;
+  ctx.beginPath();
+}
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", canclePainting);
 canvas.addEventListener("mouseleave", canclePainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
