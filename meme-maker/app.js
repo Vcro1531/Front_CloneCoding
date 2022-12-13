376f56f4d7fe813a98b6406da24c1d1b0ec4dafd
Vcro1531 <jiminkim0520@gmail.com>
2022-12-13 20:23:24 +0900

    ⚡️ :: 색 변경 기능

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -1,3 +1,4 @@
+const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -25,6 +26,10 @@ function canclePainting() {
 function onLineWidthChange(event) {
   ctx.lineWidth = event.target.value;
 }
+function onColorChange(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
@@ -32,3 +37,4 @@ canvas.addEventListener("mouseup", canclePainting);
 canvas.addEventListener("mouseleave", canclePainting);
 
 lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
