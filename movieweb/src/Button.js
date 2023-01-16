993a0b721ae233d5a7ce03b83abd97b64c1859e5
Vcro1531 <jiminkim0520@gmail.com>
2023-01-16 23:54:59 +0900

    ⚡️ :: 커밋 채우기용

--- movieweb/src/Button.js
+++ movieweb/src/Button.js
@@ -1,9 +1,7 @@
 import PropTypes from "prop-types";
 import styles from "./Button.module.css";
 
-function Button({ text }) {
-  return <button className={styles.btn}>{text}</button>;
-}
+function Button({ text }) {}
 
 Button.propTypes = {
   text: PropTypes.string.isRequired,
