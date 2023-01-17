a7f834f2d3b8aea33aad6a06539bdc96c8d357c6
Vcro1531 <jiminkim0520@gmail.com>
2023-01-17 22:08:45 +0900

    ⚡️ :: useEffect 기초

--- movieweb/src/App.js
+++ movieweb/src/App.js
@@ -1,9 +1,15 @@
-import { useState } from "react";
-import Button from "./Button";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("i run only once");
+  };
+  useEffect(() => {
+    console.log("CALL THE API");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
