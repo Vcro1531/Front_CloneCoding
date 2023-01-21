f8efe9f8eb0d125fb1dc9004e9b9379391f0d029
Vcro1531 <jiminkim0520@gmail.com>
2023-01-21 23:48:47 +0900

    ⚡️ :: cleanUp(1)

--- movieweb/src/App.js
+++ movieweb/src/App.js
@@ -1,10 +1,23 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+  function hiFn() {
+    console.log("created");
+    return byeFn;
+  }
+  function byeFn() {
+    console.log("destroyed");
+  }
+  useEffect(hiFn, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
   const [showing, setShowing] = useState(false);
   const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
+      {showing ? <Hello /> : null}
       <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
