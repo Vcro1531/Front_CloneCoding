5ac5e62072281de02bedd5527605411c46a7f88a
Vcro1531 <jiminkim0520@gmail.com>
2023-01-21 23:33:38 +0900

    ⚡️ :: 커밋채우기

--- movieweb/src/App.js
+++ movieweb/src/App.js
@@ -2,9 +2,10 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <button>{showing ? "Hide" : "Showing"}</button>
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
