2aae9247815b7febefef82db57cb9887827465f2
Vcro1531 <jiminkim0520@gmail.com>
2023-01-26 22:20:54 +0900

    ⚡️ :: Movie App setting 1

--- movieweb/src/App.js
+++ movieweb/src/App.js
@@ -2,28 +2,13 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
+    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year
+    `)
       .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+      .then((json) => console.log(json));
   }, []);
-  return (
-    <div>
-      <h1>The Coins{loading ? "" : `(${coins.length})`}</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((coin) => (
-          <li key={coin.id}>
-            {coin.name}({coin.symbol} : ${Math.round(coin.quotes.USD.price)} USD)
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div>{loading ? <h3>Loading...</h3> : null}</div>;
 }
 
 export default App;
