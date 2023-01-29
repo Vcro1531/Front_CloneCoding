46b2388c6956ded52616c42f43bfb02c83404c3b
Vcro1531 <jiminkim0520@gmail.com>
2023-01-29 21:01:49 +0900

    ⚡️ :: async&await / movie title 출력

--- movieweb/src/App.js
+++ movieweb/src/App.js
@@ -3,17 +3,31 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year
+    `);
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year
-    `)
-      .then((response) => response.json())
-      .then((json) => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
+
   console.log(movies);
-  return <div>{loading ? <h3>Loading..</h3> : null}</div>;
+  return (
+    <div>
+      {loading ? (
+        <h3>Loading..</h3>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>{movie.title}</div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
