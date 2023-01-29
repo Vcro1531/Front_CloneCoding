c257996b6b1adb95a5ad35b4a78107d4b3aa4f4c
Vcro1531 <jiminkim0520@gmail.com>
2023-01-29 20:50:32 +0900

    ⚡️ :: async&await 사용 없이

--- movieweb/src/App.js
+++ movieweb/src/App.js
@@ -2,12 +2,17 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
     fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year
     `)
       .then((response) => response.json())
-      .then((json) => console.log(json));
+      .then((json) => {
+        setMovies(json.data.movies);
+        setLoading(false);
+      });
   }, []);
+  console.log(movies);
   return <div>{loading ? <h3>Loading..</h3> : null}</div>;
 }
 
