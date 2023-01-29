e48ca61b5d52c4d3778ff1e090c3dab4d74846bd
Vcro1531 <jiminkim0520@gmail.com>
2023-01-29 21:27:31 +0900

    ⚡️ :: movie_poster 표시

--- movieweb/src/App.js
+++ movieweb/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const getMovies = async () => {
-    const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year
+    const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year
     `);
     const json = await response.json();
     setMovies(json.data.movies);
@@ -23,6 +23,7 @@ function App() {
         <div>
           {movies.map((movie) => (
             <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
               <h1>{movie.title}</h1>
               <p>{movie.summary}</p>
               <ul>
