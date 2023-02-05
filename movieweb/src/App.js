5ffc8961e9e60d5d1c21e08743dccbe5f876db4a
Vcro1531 <jiminkim0520@gmail.com>
2023-02-05 22:09:48 +0900

    ⚡️ :: App -> Home

--- movieweb/src/App.js
+++ movieweb/src/App.js
@@ -1,34 +1,5 @@
-import { useEffect, useState } from "react";
-import Movie from "./Movie";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year
-    `);
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  console.log(movies);
-  return (
-    <div>
-      {loading ? (
-        <h3>Loading..</h3>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <Movie key={movie.id} coverImg={movie.medium_cover_image} title={movie.title} summary={movie.summary} genres={movie.genres} />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
